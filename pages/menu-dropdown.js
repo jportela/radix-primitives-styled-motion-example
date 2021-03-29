c15a7d502bb3d6528631ca8c4ce1a2a32689ccdd
Joao Portela <joao.portela@gmail.com>
2021-03-29 10:25:33 +0100

    add hover state

--- pages/menu-dropdown.js
+++ pages/menu-dropdown.js
@@ -6,12 +6,17 @@ import { useState } from "react";
 import styled from "styled-components";
 
 const Button = styled(Menu.Trigger)`
-    font-size: 1.3rem;
-    color: white;
-    background-color: orangered;
-    border: none;
-    padding: 5px 10px;
-`
+  font-size: 1.3rem;
+  color: white;
+  background-color: hsl(19deg 93% 57%);
+  border: none;
+  border-radius: 10px;
+  padding: 5px 10px;
+
+  &:hover {
+    background-color: hsl(19deg 93% 64%);
+  }
+`;
 
 const Content = styled(Menu.Content)`
   min-width: 150px;
@@ -26,31 +31,39 @@ const Item = styled(Menu.Item)`
   font-size: 1.1rem;
   padding: 5px 10px;
   cursor: default;
-  &:hover, &:focus {
-      outline: 0;
-      background: rgba(0,0,0, 0.1)
+  &:hover,
+  &:focus {
+    outline: 0;
+    background: rgba(0, 0, 0, 0.1);
   }
 `;
 
 export default function MenuDropdown() {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   return (
-      <Menu.Root open={open} onOpenChange={setOpen}>
-        <Button>Open</Button>
-        <AnimatePresence>
-            {open ? 
-        <Content
-          forwardedAs={motion.div}
-          initial={{ scale: 0, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1, transition: { type: 'spring', duration: 0.4 } }}
-          exit={{ scale: 0, opacity: 0, transition: { duration: 0.2 } }}
-          forceMount
-        >
-          <Item onSelect={() => console.log("Cut")}>Cut</Item>
-          <Item onSelect={() => console.log("Copy")}>Copy</Item>
-          <Item onSelect={() => console.log("Paste")}>Paste</Item>
-        </Content> : null}
-        </AnimatePresence>
-      </Menu.Root>
+    <Menu.Root open={open} onOpenChange={setOpen}>
+      <Button forwardedAs={motion.button} whileTap={{ scale: 0.9 }}>
+        Open
+      </Button>
+      <AnimatePresence>
+        {open ? (
+          <Content
+            forwardedAs={motion.div}
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{
+              scale: 1,
+              opacity: 1,
+              transition: { type: "spring", duration: 0.3 },
+            }}
+            exit={{ scale: 0, opacity: 0, transition: { duration: 0.1 } }}
+            forceMount
+          >
+            <Item onSelect={() => console.log("Cut")}>Cut</Item>
+            <Item onSelect={() => console.log("Copy")}>Copy</Item>
+            <Item onSelect={() => console.log("Paste")}>Paste</Item>
+          </Content>
+        ) : null}
+      </AnimatePresence>
+    </Menu.Root>
   );
 }
