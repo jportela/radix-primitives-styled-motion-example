0ff6da3d314e38a02d8fd94ede9f803bd1e98899
Joao Portela <joao.portela@gmail.com>
2021-03-29 09:54:27 +0100

    adds first iteration of the menu dropdown

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 
+import { IdProvider } from '@radix-ui/react-id'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <IdProvider><Component {...pageProps} /></IdProvider>
 }
 
 export default MyApp
